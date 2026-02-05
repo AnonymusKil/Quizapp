e7accfa8e341436f6e607f15a28aea322f7d9b70
Anonymuskil <navadesignz11@gmail.com>
2026-02-05 08:33:21 -0800

    added submit

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import { addQuestion, getQuizQuestions, submitQuiz } from "../controller/Question.js";
 router.post('/addquestion', addQuestion);
 router.get("/get", getQuizQuestions);
+router.post("/submit", submitQuiz);
 
 export default router;
